11b9418817e2dbdfc2919d17278861ea8e32edc9
Christian Glassiognon <christian.glassiognon@techtonic.com>
2021-01-18 14:05:14 -0600

    Setup airbnb eslint, installed auth0, established error boundary and auth0 provider.

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,9 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import ErrorBoundary from "./errorboundary";
+import { Auth0Provider } from "@auth0/auth0-react"
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Auth0Provider
+    domain="Test"
+    clientId="ID"
+    redirectUri={window.location.origin}
+    >
+    <ErrorBoundary>
+      <App />
+    </ErrorBoundary>
+  </Auth0Provider>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
