e34a32f2e171893c003f35888ecf2788302674c2
Christian Glassiognon <christian.glassiognon@techtonic.com>
2021-01-21 18:00:50 -0600

    AVBBB-47 Added Google Maps to Find Games. https://techtonicgroup.atlassian.net/browse/AVBBB-47

--- client/src/components/game-item.jsx
+++ client/src/components/game-item.jsx
@@ -1,3 +1,4 @@
+import { Loader } from "@googlemaps/js-api-loader";
 import {
   IonCard,
   IonCardContent,
@@ -9,11 +10,38 @@ import {
 import React, { useState } from "react";
 
 const GameItem = ({ game }) => {
+  console.log(game);
   const [show, setShow] = useState(false);
+  let map;
+  let marker;
+  const loader = new Loader({
+    apiKey: "AIzaSyDcIytfVAvhm1Ia51mHHxDMaq-DUuaSlZE",
+    version: "weekly",
+  });
+
+  const loadMap = () => {
+    loader.load().then(
+      () => {
+        map = new window.google.maps.Map(document.getElementById("map"), {
+          center: game.location,
+          zoom: 15,
+          gestureHandling: "cooperative",
+        });
+        marker = new window.google.maps.Marker({
+          position: game.location,
+          map: map,
+        })
+      }
+    );
+  };
+
   return (
     <IonItem>
       <IonCard
         onClick={() => {
+          if (!show) {
+            loadMap();
+          }
           setShow(!show);
         }}
       >
@@ -24,9 +52,7 @@ const GameItem = ({ game }) => {
         </IonCardHeader>
         <IonCardContent>
           <h2>Open Seats: {game.availableSeats}</h2>
-          <h3>
-            Date: {game.date}
-          </h3>
+          <h3>Date: {game.date}</h3>
           <h4>Location: {game.address}</h4>
           {show ? <p>{game.description}</p> : undefined}
         </IonCardContent>
